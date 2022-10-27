5b6a5811dc6dd683aa6139df590c4ecdd895a9f0
swaroopcharan <85725026+swaroopcharan@users.noreply.github.com>
2022-10-27 11:33:09 +0530

    Create Code-Optimization.c

---
+++ Code-Optimization.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int fact = 1,i,num;#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int fact = 1,i,num;
+    printf("Enter the number : ");
+    scanf("%d",&num);
+    for(i = 1;i<=num;i++){
+        fact = fact * i;
+        int b = 8725;
+        int a = b*9;
+    }
+    printf("Factorial of %d is : %d",num,fact);
+    return 0;
+}
+
+//In the above code the dead code in the lines 10, 11 are not required to run the code
+//These lines can be eliminated to optimize the code
+//While comparing for loop and do while loop "for loop" is the best when comparing the time complexity
+//In for loop the condition is first checked and if it fails the code will be terminated decreasing the runtime
+//In do-while loop as the statement is executed first the run time will be differed or rather increased if the condition fails after the execution of statement
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int fact = 1,i=1,num;
+    printf("Enter the number : ");
+    scanf("%d",&num);
+    do{
+        fact = fact *i;
+        i++;
+    }while(i<=num);
+    printf("Factorial of %d is : %d",num,fact);
+    return 0;
+}
+    printf("Enter the number : ");
+    scanf("%d",&num);
+    for(i = 1;i<=num;i++){
+        fact = fact * i;
+        int b = 8725;
+        int a = b*9;
+    }
+    printf("Factorial of %d is : %d",num,fact);
+    return 0;
+}
+
+//In the above code the dead code in the lines 10, 11 are not required to run the code
+//These lines can be eliminated to optimize the code
+//While comparing for loop and do while loop "for loop" is the best when comparing the time complexity
+//In for loop the condition is first checked and if it fails the code will be terminated decreasing the runtime
+//In do-while loop as the statement is executed first the run time will be differed or rather increased if the condition fails after the execution of statement
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int fact = 1,i=1,num;
+    printf("Enter the number : ");
+    scanf("%d",&num);
+    do{
+        fact = fact *i;
+        i++;
+    }while(i<=num);
+    printf("Factorial of %d is : %d",num,fact);
+    return 0;
+}
